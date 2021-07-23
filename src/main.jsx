b425631adab5ccac2c4ed6c5e15b7258904fe918
Harvey <harvey0379@163.com>
2021-07-24 02:41:07 +0800

    ➕ antd

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ConfigProvider } from 'antd';
+import 'antd/dist/antd.less';
+import zhCN from 'antd/lib/locale/zh_CN';
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ConfigProvider locale={zhCN}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </ConfigProvider>,
+  document.getElementById('root'),
+);
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
+import { getThemeVariables } from 'antd/dist/theme';
+import themeVariables from './config/macaron';
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/vite-react-antd-starter/',
   plugins: [reactRefresh()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: {
+          ...getThemeVariables({
+            // dark: true, // Enable dark mode
+            // compact: true, // Enable compact mode
+          }),
+          ...themeVariables,
+        },
+      },
+    },
+  },
 });
