9f15f84eb88f78a88d25376fa3833776355bd68a
Harvey <harvey0379@163.com>
2021-07-24 23:18:48 +0800

    👷 cdn

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,32 @@ import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
 import { getThemeVariables } from 'antd/dist/theme';
+import commonjs from 'rollup-plugin-commonjs';
+import externalGlobals from 'rollup-plugin-external-globals';
+import { injectHtml } from 'vite-plugin-html';
 import themeVariables from './config/macaron';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/vite-react-antd-starter/',
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    injectHtml({
+      injectData: {
+        title: 'vite-react-antd-starter',
+        injectScript: [
+          process.env.NODE_ENV === 'production' &&
+            [
+              `<script crossorigin src="https://unpkg.com/react@17/umd/react.production.min.js"></script>`,
+              `<script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>`,
+              `<script src="https://cdnjs.cloudflare.com/ajax/libs/antd/4.16.8/antd.min.js" integrity="" crossorigin="anonymous" referrerpolicy="no-referrer"></script>`,
+            ].join(''),
+        ]
+          .filter(Boolean)
+          .join(''),
+      },
+    }),
+  ],
   css: {
     preprocessorOptions: {
       less: {
@@ -27,5 +47,18 @@ export default defineConfig({
       `import React, { useState } from 'react';`,
       `import { ConfigProvider, DatePicker, message, Alert, Button } from 'antd';`,
     ].join(''),
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-dom', 'react', 'antd'],
+      plugins: [
+        commonjs(),
+        externalGlobals({
+          'react-dom': 'ReactDOM',
+          react: 'React',
+          antd: 'antd',
+        }),
+      ],
+    },
   },
 });
