1b2ce0945728702ea7d72e8aad0420023e081a50
Huauauaa <Huauauaa@users.noreply.github.com>
2021-07-26 03:20:54 +0000

    deploy: e07172478a701b331749b57f6b6ef87dd5715bba

--- assets/AboutView.74f825b0.js
+++
@@ -1,1 +0,0 @@
-import"./index.7d7eb5fb.js";import"./vendor.1d324fc8.js";function e(){const[e]=React.useState("About");return React.createElement(React.Fragment,null,React.createElement("h2",null,e))}export default e;
